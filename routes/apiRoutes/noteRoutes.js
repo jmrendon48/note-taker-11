cccce91114737f156c6098a8be0315e41431b08e
AlexDeLarge <jmrendon48@gmail.com>
2021-03-02 21:52:59 -0800

    separated functions into their own files

---
+++ routes/apiRoutes/noteRoutes.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const {findById, createNewNote, validateNote} = require('../../lib/notes');
+const { notes } = require('../../db/db.json');
+const router = require('express').Router();
+
+router.get('/notes', (req, res) => {
+    res.json(notes);
+  });
+
+router.get('/api/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
+
+router.post('/notes', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
+    
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted.');
+    } else {
+        const note = createNewNote(req.body, notes);
+        res.json(notes);
+  }
+});
+
+router.delete("/notes/:id", (req, res) => {
+    const id = req.params.id;
+    notes.forEach((array, index) => {
+        if (id == array.id) {    
+            notes.splice(index, 1);
+            const newNotes = notes.slice();
+            const jsonNotes = JSON.stringify({ notes: newNotes }, null, 2)
+            fs.writeFile("./db/db.json", jsonNotes, function(err) {
+                if (err) {
+                    return console.log(err);
+                }
+            });
+        }
+    });
+    res.json(true);
+});
+
+module.exports  = router;
--- server.js
+++ server.js
@@ -2,90 +2,19 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
+const { notes } = require('./db/db.json');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 app.use(express.static('public'));
-const { notes } = require('./db/db.json');
-
-function findById(id, notesArray) {
-    const result = notesArray.filter(note => note.id === id)[0];
-    return result;
-};
-
-function createNewNote(body, notesArray) {
-    const note = body;
-    notesArray.push(note);
-    fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify({ notes: notesArray }, null, 2)
-    );
-    return note;
-};
-
-function validateNote(note) {
-    if (!note.title || typeof note.title !== 'string') {
-        return false;
-    }
-    if (!note.text || typeof note.text !== 'string') {
-        return false;
-    }
-    return true;
-};
-
-app.get('/api/notes', (req, res) => {
-    res.json(notes);
-  });
-
-app.get('/api/notes/:id', (req, res) => {
-    const result = findById(req.params.id, notes);
-    if (result) {
-        res.json(result);
-    } else {
-        res.send(404);
-    }
-});
-
-app.post('/api/notes', (req, res) => {
-    // set id based on what the next index of the array will be
-    req.body.id = notes.length.toString();
-    
-    // if any data in req.body is incorrect, send 400 error back
-    if (!validateNote(req.body)) {
-        res.status(400).send('The note is not properly formatted.');
-    } else {
-        const note = createNewNote(req.body, notes);
-        res.json(notes);
-  }
-});
-
-app.delete("/api/notes/:id", (req, res) => {
-    const id = req.params.id;
-    notes.forEach((array, index) => {
-        if (id == array.id) {    
-            notes.splice(index, 1);
-            const newNotes = notes.slice();
-            const jsonNotes = JSON.stringify({ notes: newNotes }, null, 2)
-            fs.writeFile("./db/db.json", jsonNotes, function(err) {
-                if (err) {
-                    return console.log(err);
-                }
-            });
-        }
-    });
-    res.json(true);
-});
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-  });
-
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+  
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
